a1329bab3c4de1e1d179f6024e57dd2c64d58f58
Dzmitry Shmat <d.shmat.contact@gmail.com>
2024-10-20 21:31:19 +0400

    feat: introduce Input()

--- code-personal/starting-project/src/app/user/user.component.ts
+++ code-personal/starting-project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import {Component, computed, signal} from '@angular/core';
-
-import {DUMMY_USERS} from '../dummy-users';
-
-const RANDOM_INDEX = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,17 +8,16 @@ const RANDOM_INDEX = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[RANDOM_INDEX]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
-  /*
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
   }
-  */
+
 
   onSelectUser() {
-    const RANDOM_INDEX = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[RANDOM_INDEX]);
+
   }
 }
