07f4991300c46368d90511965d9b0a06d758e3d7
Dzmitry Shmat <d.shmat.contact@gmail.com>
2024-10-21 10:47:05 +0400

    "feat: add user type to user.component"

--- code-personal/starting-project/src/app/user/user.component.ts
+++ code-personal/starting-project/src/app/user/user.component.ts
@@ -8,17 +8,19 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string,
+    name: string,
+    avatar: string
+  };
   @Output() selectedUser = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
 
   onSelectUser() {
-    this.selectedUser.emit(this.id);
+    this.selectedUser.emit(this.user.id);
   }
 }
