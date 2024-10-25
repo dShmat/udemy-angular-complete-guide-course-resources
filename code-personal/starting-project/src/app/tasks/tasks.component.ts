dd227d354ac4aacfa74eeefae19db8a5ca66a359
Dzmitry Shmat <d.shmat.contact@gmail.com>
2024-10-26 01:00:31 +0400

    feat: functionality to complete tasks

--- code-personal/starting-project/src/app/tasks/tasks.component.ts
+++ code-personal/starting-project/src/app/tasks/tasks.component.ts
@@ -41,6 +41,10 @@ export class TasksComponent {
   ]
 
   get selectedUserTasks() {
-    return this.tasks.filter((task)=> task.userId === this.userId);
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
