5e49dd34cdb6eeaace14dae6b4fd9c79e6acf62f
Dzmitry Shmat <d.shmat.contact@gmail.com>
2024-10-26 00:35:33 +0400

    feat: add model files

--- code-personal/starting-project/src/app/tasks/task/task.component.ts
+++ code-personal/starting-project/src/app/tasks/task/task.component.ts
@@ -1,12 +1,5 @@
-import {Component, Input} from '@angular/core';
-
-interface Task {
-  id: string,
-  userId: string,
-  title: string,
-  summary: string,
-  dueDate: string,
-}
+import { Component, Input } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-task',
