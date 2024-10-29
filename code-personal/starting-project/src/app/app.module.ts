53b0f9cac629a71d15b3272dca9b4e9a7c455b12
Dzmitry Shmat <d.shmat.contact@gmail.com>
2024-10-29 16:00:55 +0400

    feat: add app module

---
+++ code-personal/starting-project/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+import {RouterOutlet} from "@angular/router";
+import {HeaderComponent} from "./header/header.component";
+import {UserComponent} from "./user/user.component";
+import {TasksComponent} from "./tasks/tasks.component";
+import {BrowserModule} from "@angular/platform-browser";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, BrowserModule],
+})
+export class AppModule {
+}
